3f2a503fca5b3f50ace17fc7e41f9b8d9197e1b8
Jacob Evans <cloud887@gmail.com>
2019-12-23 14:51:01 -0600

    modules environment not working trying to use ESM

---
+++ index.js
@@ -0,0 +1,17 @@
+import { Machine, interpret } from 'xstate';
+
+// Stateless machine definition
+// machine.transition(...) is a pure function used by the interpreter.
+const toggleMachine = Machine({
+  id: 'toggle',
+  initial: 'inactive',
+  states: {
+    inactive: { on: { TOGGLE: 'active' } },
+    active: { on: { TOGGLE: 'inactive' } }
+  }
+});
+
+// Machine instance with internal state
+const toggleService = interpret(toggleMachine)
+  .onTransition(state => console.log(state.value))
+  .start();
--- src/index.js
+++
@@ -1,1 +0,0 @@
-import { Machine } from "xstate";
