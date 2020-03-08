3bc18a881dc3a27bc201d2aa0926274caaf6b95f
Jacob Evans <cloud887@gmail.com>
2020-03-08 16:58:33 -0500

    Counter MVP finished

--- src/counter/components/counter/index.js
+++ src/counter/components/counter/index.js
@@ -5,6 +5,22 @@ import { counterMachine } from "../../machines/counter-machine";
 export default function Counter() {
   const [current, send] = useMachine(counterMachine);
 
-  console.log(curent);
-  return <div />;
+  console.log(current, `Current`);
+  return (
+    <>
+      <button
+        onClick={() => send(`INCREMENT`)}
+        style={{ width: 100, height: 60 }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => send(`DECREMENT`)}
+        style={{ width: 100, height: 60 }}
+      >
+        DECREASE
+      </button>
+      <span style={{ fontSize: 100 }}>{current.context.count}</span>
+    </>
+  );
 }
